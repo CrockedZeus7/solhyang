90ae55ac208c1ee6895a58da6c0ef250bfa7737e
바보맛쿠키 <71879504+CrockedZeus7@users.noreply.github.com>
2024-12-19 00:47:07 +0900

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 const boothContainer = document.getElementById("booth-container");
 const boothCongestionMap = {};
-const socket = new WebSocket('ws://localhost:8080');
+const socket = new WebSocket('wss://solhyang.xyz');
 
 function loadBooths() {
   boothContainer.innerHTML = "";
